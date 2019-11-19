8fc6977508da2adc0564d2ef44cfa24b03edf321
Jorge Fabregas <n01338871@nj-00118-019.humber.org>
2019-11-19 12:23:11 -0500

    lab

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/******* GLOABL VARIABLES/DATA ********/
+// Define all of your variables here, including Object and Array references
 
 const allCourses = [
   {
@@ -7,50 +9,88 @@ const allCourses = [
     start: { term: `Fall`, year: 2019 },
     weeks: 15,
     breaks: true,
-    duration: 160  
-  },
-  {
-    name: `Planning and Prototype`,
+    duration: 160
+  },{
+    name: `Applied Web Development`,
+    code: `WDDM-113`,
+    instructor: `Rocco Panacci`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{
+    name: `Web Design Plan & Prototype`,
     code: `WDDM-117`,
     instructor: `Cory Coletta`,
     start: { term: `Fall`, year: 2019 },
     weeks: 15,
     breaks: true,
-    duration: 160,  
+    duration: 160
+  },{
+    name: `Web Production Techniques`,
+    code: `WDDM-116`,
+    instructor: `Mirolad Eftoski`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
   }
-]
+];
 
 
-document.getElementById(`name`).innerHTML = allCourses[1].name;
-document.getElementById(`code`).innerHTML = allCourses[1].code;
-document.getElementById(`instructor`).innerHTML = allCourses[1].instructor;
-document.getElementById(`start`).innerHTML = allCourses[1].start;
-document.getElementById(`weeks`).innerHTML = allCourses[1].weeks;
-document.getElementById(`duration`).innerHTML = allCourses[1].duration;
-
-/*
-LAB:
-1. Fix the output of the start term by turning the Object's properties into a String
-      Ex: { term: `Fall`, year: 2019 }  >>  `Fall 2019`
-2. Write a function that will take a Number of minutes and will return a human-readable String
-      Ex: 160  >>  `2 hr, 40 min`
-      Hints:
-        - Division: /
-        - Remainder: %
-        - Rounding:
-            - Standard rules:     Math.round(4.5)
-            - Always round up:    Math.ceil(1.001)
-            - Always round down:  Math.floor(1.999)
-*/
+/************* FUNCTIONS *************/
+// Now store all the functions that will manipulate the data
 
 function getDurationFromMinutes(minutes) {
-  const hrs = Math.floor (minutes/60);
-  const mins = duration - hrs * 60;
+  
+  const hrs = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  // const mins = minutes - hrs * 60; // Could also use this
 
   return `${hrs} hr, ${mins} min`;
 }
 
-getDurationFromMinutes(160);  // 2 hr, 40 min
+// Function:
+// Parameters: course:Object
+// Return: String of HTML (<article>)
+function getCourseAsHtmlString(course) {
+  return `
+    <article class="course">
+      <h3 id="name">${course.name}</h3>
+      <ul>
+        <li>Course Code: <strong>${course.code}</strong></li>
+        <li>Instructor: <strong>${course.instructor}</strong></li>
+        <li>Start: <strong>${course.start.term} ${course.start.year}</strong></li>
+        <li>
+          Weeks: <strong>${course.weeks}</strong>
+          <ul>
+            <li>Includes Break: <strong>${course.breaks}</strong></li>
+          </ul>
+        </li>
+        <li>Duration: <strong>${getDurationFromMinutes(course.duration)}</strong></li>
+      </ul>
+    </article>`;
+}
+
+/************* EXECUTABLE *************/
+// Execute functions that will access data
+
+
+// LAB:
+// 1. Add a button the document that when clicked, 
+// will load ONLY the first three courses
+// Test by having at least 4 courses
+
+// 2. Wrap the below functionality in a function
+// that takes the Array to print as a parameter
+// Test by passing "allCourses".
+
+// 3. Wrap the entire execution in an Event handler
+// so that the code only runs when the window
+// has finished loading
+
+document.getElementById('courses').innerHTML = 
+  allCourses.map(getCourseAsHtmlString).join('\n')
 
 
 
